380e23cf27fc21e35bdca7eb4fc6684f5a8bfd9e
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-01-14 21:50:24 +0100

    fix

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,28 +1,31 @@
-    function initModal() {
-        var modals = document.querySelectorAll(".modal");
-        var buttons = document.querySelectorAll(".openModal");
-        var spans = document.querySelectorAll(".close");
+function initModal() {
+    var modals = document.querySelectorAll(".modal");
+    var buttons = document.querySelectorAll(".openModal");
+    var spans = document.querySelectorAll(".close");
 
-        modals.forEach(function (modal) {
+    modals.forEach(function (modal) {
+        modal.style.display = "none";
+        console.log("1");
+    });
+
+    buttons.forEach(function (btn) {
+        btn.onclick = function () {
+            var modalId = btn.getAttribute("data-modal");
+            var modal = document.getElementById(modalId);
+            modal.style.display = "block";
+            console.log("2");
+        };
+    });
+
+    spans.forEach(function (span) {
+        span.onclick = function () {
+            var modalId = span.closest(".modal").id;
+            var modal = document.getElementById(modalId);
             modal.style.display = "none";
-        });
+            console.log("3");
+        };
+    });
+}
 
-        buttons.forEach(function (btn) {
-            btn.onclick = function () {
-                var modalId = btn.getAttribute("data-modal");
-                var modal = document.getElementById(modalId);
-                modal.style.display = "block";
-            };
-        });
-
-        spans.forEach(function (span) {
-            span.onclick = function () {
-                var modalId = span.closest(".modal").id;
-                var modal = document.getElementById(modalId);
-                modal.style.display = "none";
-            };
-        });
-    }
-
-    // Appel de la fonction pour initialiser le modal
-    initModal();
+// Appel de la fonction pour initialiser le modal
+initModal();
